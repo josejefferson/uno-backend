b15cb434b8c71fa1b84a310b650d6beaf381bb98
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2023-06-23 22:28:16 +0000

    fix: Removido códigos não utilizados

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
 const env = {
   environment: process.env.NODE_ENV,
   port: Number(process.env.BACKEND_PORT) || Number(process.env.PORT),
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-// @ts-ignore
-console._error = console.error
-console.error = (...args) => {
-  if (args[0]?.startsWith?.('Error: Not found: ')) return
-  // @ts-ignore
-  console._error(...args)
-}
-
 console.clear()
 import dotenv from 'dotenv'
 dotenv.config()
