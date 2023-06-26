da6f52e176e8d7d49aaebdd947cf4a8c9c5837f2
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2023-06-26 19:41:49 +0000

    feat: Adicionado sessionID manual

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -1,11 +1,20 @@
+import { NextFunction, Request, Response } from 'express'
 import session from 'express-session'
 
-export const expressSession = session({
-  secret: process.env.SESSION_SECRET || '@secret!',
-  saveUninitialized: true,
-  cookie: { maxAge: 1000 * 60 * 60 * 24 * 365 },
-  resave: false
-})
+export const expressSession = (req: Request, res: Response, next: NextFunction) => {
+  if (req.query.sessionID) {
+    req.sessionID = req.query.sessionID as string
+    req.session = { name: req.query.name as string, cookie: req.cookies }
+    next()
+  } else {
+    session({
+      secret: process.env.SESSION_SECRET || '@secret!',
+      saveUninitialized: true,
+      cookie: { maxAge: 1000 * 60 * 60 * 24 * 365 },
+      resave: false
+    })(req, res, next)
+  }
+}
 
 export const socketSession = (socket: any, next: any) =>
   expressSession(socket.request, {} as any, next)
